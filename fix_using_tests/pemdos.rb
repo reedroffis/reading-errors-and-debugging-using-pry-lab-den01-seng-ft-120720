6713ec3a377262ffda3b24fe590eade52b4a73d4
Reed Roffis <reedroffis18@gmail.com>
2020-11-27 21:39:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,8 @@
 require "pry" # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  binding.pry 
   if string[0] == "s"
     10 * "s" + string
-    binding.pry
   else
     string
   end
